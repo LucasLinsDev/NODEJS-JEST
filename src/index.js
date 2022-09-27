e411b2214f00fb579ef893b17cf572f8e8888305
Lucas Lins <lucaslinsperfil@gmail.com>
2022-09-27 14:48:50 -0300

    add routes users

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('nose js');
+import express from 'express';
+import cors from 'cors'
+import {routes} from './routes.js'
+
+const server=express();
+
+server.use(express.json());
+
+
+server.use(routes);
+
+server.listen(5000,()=>{
+  console.log(`Server running in http://localhost:5000`)
+})
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,26 @@
 import {Router} from 'express';
 
-const routes = Router()
+const routes = Router();
+
+const database=['Nath']
+
+routes.get('/users' , (req,res) => {
+
+  return res.status(200).json(database);
+
+});
+
+routes.post('/users' , (req,res) => {
+  
+  try{
+     const {name} =req.body
+     database.push(name);
+     return res.status(201).json(database);
+  }catch(error){
+    return res.status(500).json({message:'error'})
+  }
+
+});
 
 
-export {routes}
+export { routes }
